eb8291a4f9bd033254cbc6c9492f35a81b9898c8
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-09 20:21:46 +0000

    project configurations updated

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+// import { Footer, NavBar } from "@components";
+
+export const metadata = {
+  title: "Car Hub",
+  description: "Discover world's best car showcase application",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body className='relative'>
+        {children}
+      </body>
+    </html>
+  );
+}
