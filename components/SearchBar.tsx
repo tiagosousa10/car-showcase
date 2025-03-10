b7e7f00c59d9ab3cd3716ffffe1e0a32a901f39b
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-10 11:27:06 +0000

    add SearchManufaturer and headlessUI

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+"use client"
+
+import {useState} from 'react'
+import SearchManufacturer from './SearchManufacturer'
 
 const SearchBar = () => {
+   const [manufacturer, setManufacturer] = useState('')
+   
+   const handleSearch = () => {
+
+   }
+
   return (
-    <div>SearchBar</div>
+    <form className='searchbar' onSubmit={handleSearch}>
+      <div className='searchbar__item'>
+         <SearchManufacturer
+            manufacturer={manufacturer}
+            setManufacturer={setManufacturer}
+         />
+      </div>
+    </form>
   )
 }
 
