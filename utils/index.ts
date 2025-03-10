581d647cf305222175b312690cd61f9c96fac3c9
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-10 12:26:31 +0000

    fetching the cars with rapid api

---
+++ utils/index.ts
@@ -0,0 +1,15 @@
+export async function fetchCars() {
+   const headers = {
+      'x-rapidapi-key': '180af2010cmshea5ba1a8a19c079p12c106jsn3c408d224c2c',
+      'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+   }
+
+   const response = await fetch( 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+      headers: headers,
+   });
+
+   const result = await response.json() // get the data to JSON
+
+   return result; // return the cars
+
+}
