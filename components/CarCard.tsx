1676f3103d8de429f84d0e86f56b89e7901d3a6c
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-12 16:02:36 +0000

    fetching the car image api

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,7 +4,7 @@ import {useState} from 'react'
 import Image from 'next/image'
 import { CarProps } from '@types';
 import CustomButton from './CustomButton';
-import { calculateCarRent } from '@utils';
+import { calculateCarRent, generateCarImageUrl } from '@utils';
 import CarDetails from './CarDetails';
 
 interface CardCardProps {
@@ -36,7 +36,7 @@ const CarCard = ({car}: CardCardProps) => {
 
       <div className='relative w-full h-40 my-3 object-contain'>
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="car model"
           fill 
           priority
--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,12 @@
+import { CarProps } from "@types";
+
 export async function fetchCars() {
    const headers = {
       'x-rapidapi-key': '180af2010cmshea5ba1a8a19c079p12c106jsn3c408d224c2c',
       'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
    }
 
-   const response = await fetch( 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+   const response = await fetch( 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
       headers: headers,
    });
 
@@ -29,3 +31,19 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
    return rentalRatePerDay.toFixed(0);
  };
  
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+   const url = new URL("https://cdn.imagin.studio/getimage");
+   const { make, model, year } = car;
+ 
+   // used to 
+   url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+   url.searchParams.append('make', make);
+   url.searchParams.append('modelFamily', model.split(" ")[0]);
+   url.searchParams.append('zoomType', 'fullscreen');
+   url.searchParams.append('modelYear', `${year}`);
+   // url.searchParams.append('zoomLevel', zoomLevel);
+   url.searchParams.append('angle', `${angle}`);
+ 
+   return `${url}`;
+} 
